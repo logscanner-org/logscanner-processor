fd8e107c15e588041d7bb483180383d013fbbbe4
Obidjon_Eshmamatov <Obidjon.Eshmamatov@wolterskluwer.com>
2025-11-24 14:59:10 +0500

    initial commit

---
+++ src/main/java/com/star/logscanner/LogscannerApplication.java
@@ -0,0 +1,13 @@
+package com.star.logscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogscannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogscannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/star/logscanner/LogscannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.logscanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogscannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
